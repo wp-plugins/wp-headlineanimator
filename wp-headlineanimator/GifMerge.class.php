e063482d75bcebac3b208eb48b5cb633bdce5b95
Stargazer <Stargazer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-04-03 10:35:15 +0000

    Update to GifMerge Class 2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-headlineanimator/trunk@10104 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-headlineanimator/GifMerge.class.php
+++ wp-headlineanimator/GifMerge.class.php
@@ -1,456 +1,231 @@
 <?php
 /*
-+-------------------------------------------------+
-+                                                 +
-+   GifMaerge.class.php ver. 1.1 by László Zsidi  +
-+     examples and support on http://gifs.hu      +
-+                                                 +
-+    This class can be used and distributed       +
-+    free of charge, but cannot be modified       +
-+        without permission of author.            +
-+                                                 +
-+-------------------------------------------------+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	GIFEncoder Version 2.0 by László Zsidi, http://gifs.hu
+::
+::	This class is a rewritten 'GifMerge.class.php' version.
+::
+::  Modification:
+::   - Simplified and easy code,
+::   - Ultra fast encoding,
+::   - Built-in errors,
+::   - Stable working
+::
+::
+::	Updated at 2007. 02. 13. '00.05.AM'
+::
 */
-class GifMerge
-{
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::        Class variables - constans         ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/
-	var $ver							= '1.1';
-	var $dly							= 50;
-	var $mod							= 'C_FILE';
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::         Class variables - booleans        ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/
-	var $first							= true;
-	var $debug 							= false;
-	var $use_loop						= false;
-	var $transparent					= false;
-	var $use_global_in					= false;
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::         Class variables - integers        ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/
-	var $x								= 0;
-	var $y								= 0;
-	var $ch								= 0;
-	var $fin							= 0;
-	var $fout							= '';
-	var $loop							= 0;
-	var $delay							= 0;
-	var $width							= 0;
-	var $height							= 0;
-	var $trans1 						= 255;
-	var $trans2 						= 255;
-	var $trans3 						= 255;
-	var $disposal						= 2;
-	var $out_color_table_size			= 0;
-	var $local_color_table_flag			= 0;
-	var $global_color_table_size		= 0;
-	var $out_color_table_sizecode		= 0;
-	var $global_color_table_sizecode	= 0;
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::         Class variables - arrays          ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/
-	var $gif							= array(0x47, 0x49, 0x46);
-	var $buffer							= array();
-	var $local_in						= array();
-	var $global_in						= array();
-	var $global_out						= array();
-	var $logical_screen_descriptor		= array();
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::            Class entry point              ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
+Class GIFEncoder {
+	var $GIF = "GIF89a";		/* GIF header 6 bytes	*/
+	var $VER = "GIFEncoder V2.06";	/* Encoder version		*/
 
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::            Class constructor              ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/
-	function GifMerge($images, $t1, $t2, $t3, $loop, $dl, $xpos, $ypos, $model, $debug = false)
-	{
-		$debug ? $this->debug = true : $this->debug = false;
-		if($this->debug) print "<b>GifMerge $this->ver debugger:</b><br>\n";
-		if($model)
-			$this->mod = $model;
-		if($loop > -1)
-		{
-			$this->loop = floor($loop - 1);
-			$this->use_loop = true;
-		}
-		if($t1 > -1 && $t2 > -1 && $t3 > -1)
-		{
-			$this->trans1 = $t1;
-			$this->trans2 = $t2;
-			$this->trans3 = $t3;
-			$this->transparent = true;
-		}
-		for($i = 0; $i < count($images); $i++)
-		{
-			$dl[$i]		? $this->delay	= $dl[$i]	: $this->delay = $this->dly;
-			$xpos[$i] 	? $this->x		= $xpos[$i] : $this->x = 0;
-			$ypos[$i] 	? $this->y		= $ypos[$i] : $this->y = 0;
-			$this->start_gifmerge_process($images[$i]);
-		}
-		$this->fout .= "\x3b";
-	}
+	var $BUF = Array ( );
+	var $LOP =  0;
+	var $DIS =  2;
+	var $COL = -1;
+	var $IMG = -1;
+
+	var $ERR = Array (
+		'ERR00' =>"Does not supported function for only one image!",
+		'ERR01' =>"Source is not a GIF image!",
+		'ERR02' =>"Unintelligible flag ",
+		'ERR03' =>"Could not make animation from animated GIF source",
+	);
+
 	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::             Gifmerge processor            ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	*/	function start_gifmerge_process($fp)
-	{
-		if($this->mod == 'C_FILE')
-		{
-			if(!$this->fin = fopen($fp, 'rb'))
-			{
-				if($this->debug) print "Error file open: $fp\<br>";
-				return;
-			}
-		}
-		else if($this->mod == 'C_MEMORY')
-		{
-			$this->ch  = 0;
-			$this->fin = $fp;
-		}
-		$this->getbytes (6);
-		if(!$this->arrcmp($this->buffer, $this->gif, 3))
-		{
-			if($this->debug) print "Isn't GIF file!\n<br>";
-			return;
-		}
-		$this->getbytes (7);
-		if($this->first) $this->logical_screen_descriptor = $this->buffer;
-		$this->global_color_table_sizecode = $this->buffer[4] & 0x07;
-		$this->global_color_table_size = 2 << $this->global_color_table_sizecode;
-		if($this->buffer[4] & 0x80)
-		{
-			$this->getbytes((3 * $this->global_color_table_size));
-			for($i = 0; $i < ((3 * $this->global_color_table_size)); $i++)
-			{
-				$this->global_in[$i] = $this->buffer[$i];
-			}
-			if($this->out_color_table_size == 0)
-			{
-				$this->out_color_table_size = $this->global_color_table_size;
-				$out_color_table_sizecode = $this->global_color_table_sizecode;
-				$this->global_out = $this->global_in;
-			}
-			if($this->global_color_table_size != $this->out_color_table_size || $this->arrcmp($this->global_out, $this->global_in, (3 * $this->global_color_table_size)))
-			{
-				$this->use_global_in = true;
-			}
-		}
-		for($loop = true; $loop;)
-		{
-			$this->getbytes(1);
-			switch ($this->buffer[0])
-			{
-				case 0x21:
-					$this->read_extension();
-					break;
-				case 0x2c:
-					$this->read_image_descriptor();
-					break;
-				case 0x3b:
-					$loop = false;
-				break;
-				default:
-            		if($this->debug) printf("Unrecognized byte code 0x%x, truncating file!\n<br>", $this->buffer[0]);
-						$loop = false;
-			}
-		}
-		if($this->mod == 'C_FILE')
-			fclose($this->fin);
-	}
-	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::          Image descriptor block           ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFEncoder...
+	::
 	*/
-	function read_image_descriptor()
-	{
-		$this->getbytes(9);
-		$head = $this->buffer;
-    	$this->local_color_table_flag = ($this->buffer[8] & 0x80) ? true : false;
-    	if($this->local_color_table_flag)
-    	{
-        	$sizecode = $this->buffer[8] & 0x07;
-			$size = 2 << $sizecode;
-			$this->getbytes(3 * $size);
-			for($i = 0; $i < (3 * $size); $i++)
-			{
-        		$this->local_in[$i] = $this->buffer[$i];
-        	}
-        	if($this->out_color_table_size == 0)
-        	{
-        		$this->out_color_table_size = $size;
-        		$out_color_table_sizecode = $sizecode;
-				for($i = 0; $i < (3 * $size); $i++)
-				{
-					$this->global_out[$i] = $this->local_in[$i];
+	function GIFEncoder	(
+							$GIF_src, $GIF_dly, $GIF_lop, $GIF_dis,
+							$GIF_red, $GIF_grn, $GIF_blu, $GIF_mod
+						) {
+		if ( ! is_array ( $GIF_src ) && ! is_array ( $GIF_tim ) ) {
+			printf	( "%s: %s", $this->VER, $this->ERR [ 'ERR00' ] );
+			exit	( 0 );
+		}
+		$this->LOP = ( $GIF_lop > -1 ) ? $GIF_lop : 0;
+		$this->DIS = ( $GIF_dis > -1 ) ? ( ( $GIF_dis < 3 ) ? $GIF_dis : 3 ) : 2;
+		$this->COL = ( $GIF_red > -1 && $GIF_grn > -1 && $GIF_blu > -1 ) ?
+						( $GIF_red | ( $GIF_grn << 8 ) | ( $GIF_blu << 16 ) ) : -1;
+
+		for ( $i = 0; $i < count ( $GIF_src ); $i++ ) {
+			if ( strToLower ( $GIF_mod ) == "url" ) {
+				$this->BUF [ ] = fread ( fopen ( $GIF_src [ $i ], "rb" ), filesize ( $GIF_src [ $i ] ) );
+			}
+			else if ( strToLower ( $GIF_mod ) == "bin" ) {
+				$this->BUF [ ] = $GIF_src [ $i ];
+			}
+			else {
+				printf	( "%s: %s ( %s )!", $this->VER, $this->ERR [ 'ERR02' ], $GIF_mod );
+				exit	( 0 );
+			}
+			if ( substr ( $this->BUF [ $i ], 0, 6 ) != "GIF87a" && substr ( $this->BUF [ $i ], 0, 6 ) != "GIF89a" ) {
+				printf	( "%s: %d %s", $this->VER, $i, $this->ERR [ 'ERR01' ] );
+				exit	( 0 );
+			}
+			for ( $j = ( 13 + 3 * ( 2 << ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 ) ) ), $k = TRUE; $k; $j++ ) {
+				switch ( $this->BUF [ $i ] { $j } ) {
+					case "!":
+						if ( ( substr ( $this->BUF [ $i ], ( $j + 3 ), 8 ) ) == "NETSCAPE" ) {
+							printf	( "%s: %s ( %s source )!", $this->VER, $this->ERR [ 'ERR03' ], ( $i + 1 ) );
+							exit	( 0 );
+						}
+						break;
+					case ";":
+						$k = FALSE;
+						break;
 				}
-        	}
-    	}
-    	if($this->first)
-    	{
-			$this->first = false;
-			$this->fout .= "\x47\x49\x46\x38\x39\x61"; // <= 'GIF89a'
-			if($this->width && $this->height)
-			{
-				$this->logical_screen_descriptor[0] = $this->width & 0xFF;
-				$this->logical_screen_descriptor[1] = ($this->width & 0xFF00) >> 8;
-				$this->logical_screen_descriptor[2] = $this->height & 0xFF;
-				$this->logical_screen_descriptor[3] = ($this->height & 0xFF00) >> 8;
-			}
-			$this->logical_screen_descriptor[4] |= 0x80;
-			$this->logical_screen_descriptor[5] &= 0xF0;
-			$this->logical_screen_descriptor[6] |= $this->out_color_table_sizecode;
-			$this->putbytes($this->logical_screen_descriptor, 7);
-        	$this->putbytes($this->global_out, ($this->out_color_table_size * 3));
-			if($this->use_loop)
-			{
-				$ns[0] = 0x21;
-				$ns[1] = 0xFF;
-				$ns[2] = 0x0B;
-				$ns[3] = 0x4e;  // N
-				$ns[4] = 0x45;  // E
-				$ns[5] = 0x54;  // T
-				$ns[6] = 0x53;  // S
-				$ns[7] = 0x43;  // C
-				$ns[8] = 0x41;  // A
-				$ns[9] = 0x50;  // P
-				$ns[10] = 0x45; // E
-				$ns[11] = 0x32; // 2
-				$ns[12] = 0x2e; // .
-				$ns[13] = 0x30; // 0
-				$ns[14] = 0x03;
-				$ns[15] = 0x01;
-				$ns[16] = $this->loop & 255;
-    			$ns[17] = $this->loop >> 8;
-				$ns[18] = 0x00;
-				$this->putbytes($ns, 19);
-			}
-    	}
-  		if($this->use_global_in)
-		{
-			$outtable = $this->global_in;
-			$outsize = $this->global_color_table_size;
-			$outsizecode = $this->global_color_table_sizecode;
-		}
-		else
-		{
-			$outtable = $this->global_out;
-			$outsize = $this->out_color_table_size;
-		}
-		if ($this->local_color_table_flag)
-		{
-			if ($size == $this->out_color_table_size && !$this->arrcmp($this->local_in, $this->global_out, $size))
-			{
-				$outtable = $global_out;
-				$outsize = $this->out_color_table_size;
-			}
-			else
-			{
-				$outtable = $this->local_in;
-				$outsize = $size;
-				$outsizecode = $sizecode;
 			}
 		}
-		$use_trans = false;
-		if($this->transparent)
-		{
-			for ($i = 0; $i < $outsize; $i++)
-				if (   $outtable [3 * $i	] == $this->trans1
-					&& $outtable [3 * $i + 1] == $this->trans2
-					&& $outtable [3 * $i + 2] == $this->trans3
-				)
-					break;
-			if ($i < $outsize)
-			{
-				$transindex = $i;
-				$use_trans = true;
-			}
-			else
-			{
-				if($this->debug) print "Transparent color not found!\n<br>";
-			}
+		GIFEncoder::GIFAddHeader ( );
+		for ( $i = 0; $i < count ( $this->BUF ); $i++ ) {
+			GIFEncoder::GIFAddFrames ( $i, $GIF_dly [ $i ] );
 		}
-		if($this->delay || $use_trans)
-		{
-			$this->buffer[0] = 0x21;
-			$this->buffer[1] = 0xf9;
-			$this->buffer[2] = 0x04;
-			$this->buffer[3] = ($this->disposal << 2) + ($use_trans ? 1 : 0);
-			$this->buffer[4] = $this->delay & 0xff;
-			$this->buffer[5] = ($this->delay & 0xff00) >> 8;
-			$this->buffer[6] = $use_trans ? $transindex : 0;
-			$this->buffer[7] = 0x00;
-			$this->putbytes($this->buffer,8);
-		}
-		$this->buffer[0] = 0x2c;
-		$this->putbytes($this->buffer,1);
-		$head[0] = $this->x & 0xff;
-		$head[1] = ($this->x & 0xff00) >> 8;
-		$head[2] = $this->y & 0xff;
-		$head[3] = ($this->y & 0xff00) >> 8;
-		$head[8] &= 0x40;
-		if($outtable != $this->global_out)
-		{
-			$head[8] |= 0x80;
-			$head[8] |= $outsizecode;
-		}
-		$this->putbytes($head,9);
-		if($outtable != $this->global_out)
-		{
-			if($this->debug) print "Using local color table.\n<br>";
-			$this->putbytes($outtable, (3 * $outsize));
-		}
-		$this->getbytes(1);
-		$this->putbytes($this->buffer,1);
-		for (;;)
-		{
-			$this->getbytes(1);
-			$this->putbytes($this->buffer,1);
-			if(($u = $this->buffer[0]) == 0)
-			{
-				break;
-			}
-			$this->getbytes($u);
-			$this->putbytes($this->buffer, $u);
-    	}
+		GIFEncoder::GIFAddFooter ( );
 	}
 	/*
-	:::::::::::::::::::::::::::::::::::::::::::::::
-	::                                           ::
-	::           Image extension block           ::
-	::                                           ::
-	:::::::::::::::::::::::::::::::::::::::::::::::
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFAddHeader...
+	::
 	*/
-	function read_extension()
-	{
-    	$this->getbytes(1);
-    	switch($this->buffer[0])
-    	{
-			case 0xf9:
-				if($this->debug) print "Skipping Graphic Control Extension.\n<br>";
-	    		$this->getbytes(6);
-	   			break;
-			case 0xfe:
-				if($this->debug) print "Skipping Comment Extension.\n<br>";
-	    		for (;;)
-	    		{
-					$this->getbytes(1);
-                	if (($u = $this->buffer[0]) == 0)
-		    			break;
-					$this->getbytes($u);
-	    		}
-	    		break;
-			case 0x01:
-				if($this->debug) print "Skipping Plain Text Extension.\n<br>";
-	    		$this->getbytes(13);
-	    		for (;;)
-	    		{
-					$this->getbytes(0);
-                	if (($u = $this->buffer[0]) == 0)
-		    			break;
-					$this->getbytes($u);
-	    		}
-				break;
-       		case 0xff:
-				if($this->debug) print "Skipping Application Extension.\n<br>";
-	    		$this->getbytes(9);
-	    		$this->getbytes(3);
-	    		for (;;)
-	    		{
-					$this->getbytes(1);
-                	if (!$this->buffer[0])
-                		break;
-                	$this->getbytes($this->buffer[0]);
-            	}
-				break;
-			default:
-				if($this->debug) print "Skipping unrecognized extension.\n<br>";
-	    		for (;;)
-	    		{
-					$this->getbytes(1);
-        			if(!$this->buffer[0])
-        				break;
-					$this->getbytes($this->buffer[0]);
+	function GIFAddHeader ( ) {
+		$cmap = 0;
+
+		if ( ord ( $this->BUF [ 0 ] { 10 } ) & 0x80 ) {
+			$cmap = 3 * ( 2 << ( ord ( $this->BUF [ 0 ] { 10 } ) & 0x07 ) );
+
+			$this->GIF .= substr ( $this->BUF [ 0 ], 6, 7		);
+			$this->GIF .= substr ( $this->BUF [ 0 ], 13, $cmap	);
+			$this->GIF .= "!\377\13NETSCAPE2.0\3\1" . GIFEncoder::GIFWord ( $this->LOP ) . "\0";
+		}
+	}
+	/*
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFAddFrames...
+	::
+	*/
+	function GIFAddFrames ( $i, $d ) {
+
+		$Locals_str = 13 + 3 * ( 2 << ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 ) );
+
+		$Locals_end = strlen ( $this->BUF [ $i ] ) - $Locals_str - 1;
+		$Locals_tmp = substr ( $this->BUF [ $i ], $Locals_str, $Locals_end );
+
+		$Global_len = 2 << ( ord ( $this->BUF [ 0  ] { 10 } ) & 0x07 );
+		$Locals_len = 2 << ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 );
+
+		$Global_rgb = substr ( $this->BUF [ 0  ], 13,
+							3 * ( 2 << ( ord ( $this->BUF [ 0  ] { 10 } ) & 0x07 ) ) );
+		$Locals_rgb = substr ( $this->BUF [ $i ], 13,
+							3 * ( 2 << ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 ) ) );
+
+		$Locals_ext = "!\xF9\x04" . chr ( ( $this->DIS << 2 ) + 0 ) .
+						chr ( ( $d >> 0 ) & 0xFF ) . chr ( ( $d >> 8 ) & 0xFF ) . "\x0\x0";
+
+		if ( $this->COL > -1 && ord ( $this->BUF [ $i ] { 10 } ) & 0x80 ) {
+			for ( $j = 0; $j < ( 2 << ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 ) ); $j++ ) {
+				if	(
+						ord ( $Locals_rgb { 3 * $j + 0 } ) == ( $this->COL >>  0 ) & 0xFF &&
+						ord ( $Locals_rgb { 3 * $j + 1 } ) == ( $this->COL >>  8 ) & 0xFF &&
+						ord ( $Locals_rgb { 3 * $j + 2 } ) == ( $this->COL >> 16 ) & 0xFF
+					) {
+					$Locals_ext = "!\xF9\x04" . chr ( ( $this->DIS << 2 ) + 1 ) .
+									chr ( ( $d >> 0 ) & 0xFF ) . chr ( ( $d >> 8 ) & 0xFF ) . chr ( $j ) . "\x0";
+					break;
 				}
-    	}
-	}
-
-	/*::::::::::::::::::::::::::::::::::::::::::*/
-
-	function arrcmp($b, $s, $l)
-	{
-		for($i = 0; $i < $l; $i++)
-		{
-			if($s{$i} != $b{$i}) return false;
+			}
 		}
-		return true;
-	}
-
-	/*::::::::::::::::::::::::::::::::::::::::::*/
-
-	function getbytes($l)
-	{
-		for($i = 0; $i < $l; $i++)
-		{
-			if($this->mod == 'C_FILE')
-			{
-        		$bin = unpack('C*', fread($this->fin, 1));
-        		$this->buffer[$i] = $bin[1];
-        	}
-        	else if($this->mod == 'C_MEMORY')
-        	{
-                $bin = unpack('C*', substr($this->fin, $this->ch, 1));
-                $this->buffer[$i] = $bin[1];
-                $this->ch++;
-        	}
+		switch ( $Locals_tmp { 0 } ) {
+			case "!":
+				$Locals_img = substr ( $Locals_tmp, 8, 10 );
+				$Locals_tmp = substr ( $Locals_tmp, 18, strlen ( $Locals_tmp ) - 18 );
+				break;
+			case ",":
+				$Locals_img = substr ( $Locals_tmp, 0, 10 );
+				$Locals_tmp = substr ( $Locals_tmp, 10, strlen ( $Locals_tmp ) - 10 );
+				break;
 		}
-		return $this->buffer;
-	}
-
-	/*::::::::::::::::::::::::::::::::::::::::::*/
-
-	function putbytes($s, $l)
-	{
-		for($i = 0; $i < $l; $i++)
-		{
-			$this->fout .= pack('C*', $s[$i]);
+		if ( ord ( $this->BUF [ $i ] { 10 } ) & 0x80 && $this->IMG > -1 ) {
+			if ( $Global_len == $Locals_len ) {
+				if ( GIFEncoder::GIFBlockCompare ( $Global_rgb, $Locals_rgb, $Global_len ) ) {
+					$this->GIF .= ( $Locals_ext . $Locals_img . $Locals_tmp );
+				}
+				else {
+					$byte  = ord ( $Locals_img { 9 } );
+					$byte |= 0x80;
+					$byte &= 0xF8;
+					$byte |= ( ord ( $this->BUF [ 0 ] { 10 } ) & 0x07 );
+					$Locals_img { 9 } = chr ( $byte );
+					$this->GIF .= ( $Locals_ext . $Locals_img . $Locals_rgb . $Locals_tmp );
+				}
+			}
+			else {
+				$byte  = ord ( $Locals_img { 9 } );
+				$byte |= 0x80;
+				$byte &= 0xF8;
+				$byte |= ( ord ( $this->BUF [ $i ] { 10 } ) & 0x07 );
+				$Locals_img { 9 } = chr ( $byte );
+				$this->GIF .= ( $Locals_ext . $Locals_img . $Locals_rgb . $Locals_tmp );
+			}
 		}
+		else {
+			$this->GIF .= ( $Locals_ext . $Locals_img . $Locals_tmp );
+		}
+		$this->IMG  = 1;
 	}
+	/*
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFAddFooter...
+	::
+	*/
+	function GIFAddFooter ( ) {
+		$this->GIF .= ";";
+	}
+	/*
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFBlockCompare...
+	::
+	*/
+	function GIFBlockCompare ( $GlobalBlock, $LocalBlock, $Len ) {
 
-	/*::::::::::::::::::::::::::::::::::::::::::*/
+		for ( $i = 0; $i < $Len; $i++ ) {
+			if	(
+					$GlobalBlock { 3 * $i + 0 } != $LocalBlock { 3 * $i + 0 } ||
+					$GlobalBlock { 3 * $i + 1 } != $LocalBlock { 3 * $i + 1 } ||
+					$GlobalBlock { 3 * $i + 2 } != $LocalBlock { 3 * $i + 2 }
+				) {
+					return ( 0 );
+			}
+		}
 
-	function getAnimation()
-	{
-		return $this->fout;
+		return ( 1 );
+	}
+	/*
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GIFWord...
+	::
+	*/
+	function GIFWord ( $int ) {
+
+		return ( chr ( $int & 0xFF ) . chr ( ( $int >> 8 ) & 0xFF ) );
+	}
+	/*
+	:::::::::::::::::::::::::::::::::::::::::::::::::::
+	::
+	::	GetAnimation...
+	::
+	*/
+	function GetAnimation ( ) {
+		return ( $this->GIF );
 	}
 }
 ?>
