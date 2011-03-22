dce3aff149ebc736debc170a9b2d78881512223d
Stargazer <Stargazer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-22 22:39:00 +0000

    change versio to 3 digits
    git-svn-id: https://plugins.svn.wordpress.org/wp-headlineanimator/trunk@363487 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-headlineanimator.php
+++ wp-headlineanimator.php
@@ -8,23 +8,23 @@
  Plugin Name: WP-HeadlineAnimator
  Plugin URI: http://www.stargazer.at/projekte
  Description: Generates a graphic like the FB Headline Animator. More info in my blog posts on my.stargazer.at...
- Version: 1.7
+ Version: 1.7.0
  Author: Christoph "Stargazer" Bauer
  Author URI: http://my.stargazer.at/
- 
+
  Author URI: http://my.stargazer.at/
- 
+
  Copyright 2010 Christoph Bauer  (email : cbauer@stargazer.at)
- 
+
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License, version 2, as
  published by the Free Software Foundation.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  */
 
 // Securing against direct calls
@@ -36,7 +36,7 @@ function wpc_write() {
   if ( !get_option('wpc_font') || !file_exists(get_option('wpc_font')) ) {
 	  return 0;
   }
-  
+
   if ( !file_exists( ABSPATH . '/' .get_option('wpc_image')) ) {
 	  return 0;
   }
@@ -45,7 +45,7 @@ function wpc_write() {
   $d		= array();
   $myposts 	= get_posts('numberposts='.get_option('wpc_artnum').'&order=DESC&orderby=post_date');
   $counter	= 0;
- 
+
   foreach($myposts as $post) :
   	$text 		= $post->post_title;
   	if ( !get_option('wpc_dateformat') ) {
@@ -53,7 +53,7 @@ function wpc_write() {
 	} else {
 		$textdate	= date( get_option('wpc_dateformat'),strtotime($post->post_date));
 	}
-	
+
 	if ( function_exists('qtrans_useDefaultLanguage') ) $text = qtrans_useDefaultLanguage($text); // just to be sure
 
 // do some option handling
@@ -63,16 +63,16 @@ function wpc_write() {
 		$xmove       = 200 - (strlen($text) * 3.5);
 	} else {
 		$xmove       = 200 - (strlen($text) * 3.5 );
-	} 
-	
-// constructing the image	
-	$picture_src =  ABSPATH . '/' . get_option('wpc_image'); 
+	}
+
+// constructing the image
+	$picture_src =  ABSPATH . '/' . get_option('wpc_image');
 	$font        = get_option('wpc_font');
 	$picture     = readpic($picture_src);
 	$colarray	 = html2rgb(get_option('wpc_textcol'));
 	$color       = ImageColorAllocate( $picture, $colarray[0], $colarray[1], $colarray[2] );
-	
-	
+
+
 // imagettftext ( image, size, angle,  x,  y, color , font , text )
 	if ($text) imagettftext(  $picture,   get_option('wpc_newssize'),     0, $xmove, 58, $color, $font, $text);
 	imagettftext(  $picture,   get_option('wpc_textsize'),     0, 100, 30, $color, $font, get_option('wpc_text') );
@@ -80,7 +80,7 @@ function wpc_write() {
 	imagedestroy( $picture );
 	$counter++;
   endforeach;
-  
+
 // arrange frames
   $counter=0;
   $frame=array_reverse($frame);
@@ -88,27 +88,27 @@ function wpc_write() {
 	$i[$counter] = array_pop($frame);
 	$counter++;
 	$i[$counter] = gif2string(readpic($picture_src));
-	$counter++; 
-	
+	$counter++;
+
 // compute the frame handling delays.
 		array_push ($d, get_option('wpc_pictime'), get_option('wpc_nopictime'));
   endforeach;
 
 
 
-/* 
-        GIFEncoder constructor: 
-        ======================= 
- 
-        image_stream = new GIFEncoder    ( 
-                            URL or Binary data    'Sources' 
-                            int                    'Delay times' 
-                            int                    'Animation loops' 
-                            int                    'Disposal' 
-                            int                    'Transparent red, green, blue colors' 
-                            int                    'Source type' 
-                        ); 
-*/ 
+/*
+        GIFEncoder constructor:
+        =======================
+
+        image_stream = new GIFEncoder    (
+                            URL or Binary data    'Sources'
+                            int                    'Delay times'
+                            int                    'Animation loops'
+                            int                    'Disposal'
+                            int                    'Transparent red, green, blue colors'
+                            int                    'Source type'
+                        );
+*/
 
     $anim = new GIFEncoder ( 	$i,
 				$d,
@@ -124,12 +124,12 @@ function wpc_write() {
 // we have to write it anyways
 	$f = fopen( ABSPATH . '/' . get_option('wpc_target').'.gif' , "w");
 	fwrite($f, $animgif);
-	
-	
+
+
 	if ( get_option('wpc_remotetarget')=='on' ) {
 		export2ftp( ABSPATH . '/' . get_option('wpc_target').'.gif' );
 	}
-	
+
 	fclose($f);
 }
 
@@ -138,7 +138,7 @@ function readpic($picture_src) {
 	* Picture loader. Life is like a box of chocolate - we don't know what we'll get.
 	* So let's pretend the user knows what he's doing, using valid extensions...
 	**/
-	
+
 	$extension = strtolower(strrchr($picture_src, '.'));
 	switch ($extension) {
 		case '.gif':
@@ -153,27 +153,27 @@ function readpic($picture_src) {
 			break;
 		default:
 			wp_die('Unknown image format');
-				
+
 	}
 	return $picture;
 }
 
 function export2ftp($src_image) {
-	/** 
+	/**
 	 *  Some people want to have the animator offsite - here's the transfer
 	 **/
 	if ( !get_option('wpc_ftp_server') ) return;
-	$ftpcon = ftp_connect(get_option('wpc_ftp_server')); 
+	$ftpcon = ftp_connect(get_option('wpc_ftp_server'));
 	$login_result = ftp_login($ftpcon, get_option('wpc_ftp_user'), get_option('wpc_ftp_pass'));
-	if ((!$ftpcon) || (!$login_result)) { 
+	if ((!$ftpcon) || (!$login_result)) {
 		echo '<div class="wrap"><h2>' . __('Error', 'wp-headlineanimator') . '!</h2><h3><font color="red">' . __('FTP connection has failed! Check Hostname and Login!', 'wp-headlineanimator') . '</font></h3></div>';
-		exit; 
-	} 
+		exit;
+	}
 	//        ftp_fput($conn_id, $file                      , $fp         , FTP_ASCII)
-	$upload = ftp_put($ftpcon, '/'.get_option('wpc_ftp_target').'.gif' , $src_image, FTP_BINARY); 
+	$upload = ftp_put($ftpcon, '/'.get_option('wpc_ftp_target').'.gif' , $src_image, FTP_BINARY);
 	if (!$upload) {
 		echo '<div class="wrap"><h2>' . __('Error', 'wp-headlineanimator') . '!</h2><h3><font color="red">' . __('FTP upload has failed!', 'wp-headlineanimator') . '</font></h3></div>';
-	} 
+	}
 	ftp_close($ftpcon);
 }
 
@@ -181,7 +181,7 @@ function gif2string($image) {
 	/**
 	 * catch the output of imagegif as we need gif images to pass it to the MergerClass
 	 **/
-	
+
 	ob_start();
 	$contents = ob_get_contents();
 	if ($contents !== false) ob_clean(); else ob_start();
@@ -255,7 +255,7 @@ function wpc_options_page() {
 	  if ($_POST['wpc_ftp_pass']) update_option('wpc_ftp_pass', $_POST['wpc_ftp_pass']);
 	  if ($_POST['wpc_ftp_path']) update_option('wpc_ftp_path', $_POST['wpc_ftp_path']);
 	  if ($_POST['wpc_ftp_target']) update_option('wpc_ftp_target', $_POST['wpc_ftp_target']);
-	  
+
 	  if ($_POST['wpc_textsize'] && intval($_POST['wpc_textsize']) > 0 ) {
 		  update_option('wpc_textsize', intval($_POST['wpc_textsize']));
 	  } else {
@@ -281,7 +281,7 @@ function wpc_options_page() {
 	  } else {
 		  update_option('wpc_nopictime', 50);
 	  }
-	  
+
 	  update_option('wpc_ftp_server', $_POST['wpc_ftp_server']);
 	  update_option('wpc_remotetarget', $_POST['wpc_remotetarget']);
 	  update_option('wpc_mode', $_POST['wpc_mode']);
@@ -296,11 +296,11 @@ function wpc_textdomain() {
 	if (function_exists('load_plugin_textdomain')) {
 		if ( !defined('WP_PLUGIN_DIR') ) {
 			load_plugin_textdomain('wp-headlineanimator', str_replace( ABSPATH, '', dirname(__FILE__) ) . '/languages');
-			
+
 		} else {
 			load_plugin_textdomain('wp-headlineanimator', false, dirname( plugin_basename(__FILE__) ) . '/languages');
 		}
-		
+
 	}
 }
 
